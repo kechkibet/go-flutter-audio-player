300c5d39ca431f3dbf8823fedaf6dae979cc12f6
JOHNPETER KIBET KECH <peter.kech@tracom.co.ke>
2021-03-08 21:59:39 +0300

    added error catching

--- audioplayer.go
+++ audioplayer.go
@@ -27,36 +27,37 @@ func (p *AudioPlayer) InitPlugin(messenger plugin.BinaryMessenger) error {
 func handlePlayAudio(arguments interface{}) (reply interface{}, err error) {
 	argsMap := arguments.(map[interface{}]interface{})
 	url := argsMap["url"].(string)
-	return playAudio(url), nil
+	resp, err := playAudio(url)
+	return resp, err
 }
 
-func playAudio(url string) bool {
+func playAudio(url string) (bool, error) {
 	//const url1 = "https://web-mall-cdn.ams3.digitaloceanspaces.com/8180d349-9269-4958-8929-fb025041be90?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=Q6MAXU4EJUULDWY2OYJT%2F20210307%2Fams3%2Fs3%2Faws4_request&X-Amz-Date=20210307T181541Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=92514ecbbff1cc1457946bf2777968ba2a566ccdc8ec099afa682b66a92248eb"
 	//f, err := os.Open("gunshot.mp3")
 	r, _ := req.Get(url)
 	err := r.ToFile("message.wav")
 	if err != nil {
 		log.Fatal(err)
-		return false
+		return false, err
 	}
 
 	f, err := os.Open("message.wav")
 
 	if err != nil {
 		log.Fatal(err)
-		return false
+		return false, err
 	}
 
 	streamer, format, err := wav.Decode(f)
 	if err != nil {
 		log.Fatal(err)
-		return false
+		return false, err
 	}
 
 	err = speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/5))
 	if err != nil {
 		log.Fatal(err)
-		return false
+		return false, err
 	}
 
 	buffer := beep.NewBuffer(format)
@@ -64,10 +65,10 @@ func playAudio(url string) bool {
 	err = streamer.Close()
 	if err != nil {
 		log.Fatal(err)
-		return false
+		return false, err
 	}
 
 	shot := buffer.Streamer(0, buffer.Len())
 	speaker.Play(shot)
-	return true
+	return true, nil
 }
